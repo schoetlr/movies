7a7e129196541a771f06a31aabacf97491417c72
schoetlr <srlrs20020@gmail.com>
2016-09-08 19:41:18 -0400

    movie-form

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,4 +7,12 @@ class LinksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def extract_linkable
+    resource, id = request.path.split('/')[1,2]
+
+    resource.singularize.classify.constantize.find(id)
+  end
 end
