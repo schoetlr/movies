b545c57306f8d07d4dcdc5f2f89bb0f7d9f6820d
schoetlr <srlrs20020@gmail.com>
2016-09-08 19:55:36 -0400

    create movies controller and view set up

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,30 @@
 class MoviesController < ApplicationController
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:success] = "Movie successfully created"
+      redirect_to movies_path
+    else
+      flash[:error] = "Could not create movie"
+      render :new
+    end
   end
 
   def index
+    @movies = Movie.order(created_at: "desc")
   end
 
   def show
   end
 
   def edit
+  end
+
+  def movie_params
+    params.require(:movie).permit(:name, :description)
   end
 end
